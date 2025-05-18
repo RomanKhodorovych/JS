1be28095a91456b92b69d6adaf16f6c3f34d0723
rkhod <r.khodorovych@gmail.com>
2025-05-18 21:36:20 +0300

    hw9 updates

--- HomeWork_9/task9.js
+++ HomeWork_9/task9.js
@@ -18,8 +18,8 @@ function calcArrProduct(arr) {
     return new Promise((resolve, reject) => {
       for (let i = 0; i < arr.length; i++) {
         if (typeof arr[i] !== 'number') {
-            const result = "Error! Incorrect array!";
-            resolve(result);
+            const result = new Error("Error! Incorrect array!");
+            reject(result);
         }
       }
       const result = arr.reduce((acc, num) => acc * num, 1);
@@ -31,3 +31,28 @@ calcArrProduct([3,4,5]).then(result => console.log(result)); // 60
 calcArrProduct([5,"user2", 7, 12]).then(result => console.log(result)); // "Error! Incorrect array!"
 
 // ************** task 3 ************** //
+
+const delay = (i, time) => new Promise(resolve => setTimeout(() => resolve(i), time));
+function showNumbers() {
+	for(let i=0, p= Promise.resolve(0); i<=10; i++){
+        const time = Math.floor(Math.random() * 4) *1000;
+        p = p.then(() => delay(i, time)).then((result) => {
+            console.log(`result = ${result}, delay = ${time/1000} sec`);
+        });
+    }
+}
+
+showNumbers();
+
+// ************** task 4 ************** //
+
+const delay = (i, time) => new Promise(resolve => setTimeout(() => resolve(i), time));
+async function showNumbers() {
+	for(let i=0; i<=10; i++){
+        const time = Math.floor(Math.random() * 4) *1000;
+        await delay(i, time);
+            console.log(`result = ${i},delay = ${time/1000} sec`);
+    }
+}
+
+showNumbers();
